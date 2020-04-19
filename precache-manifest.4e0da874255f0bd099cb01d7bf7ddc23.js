7942dbd818cf5713650a52a300e7c427255a4cac
Garvit <heisGarvit@gmail.com>
2020-04-19 13:40:48 +0530

    init

---
+++ precache-manifest.4e0da874255f0bd099cb01d7bf7ddc23.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dedeb66e5028d92a41ebcbca00693afb",
+    "url": "/nes/index.html"
+  },
+  {
+    "revision": "3f6c6fb8d1a62a32d5b0",
+    "url": "/nes/static/css/main.a2287ade.chunk.css"
+  },
+  {
+    "revision": "3d0d013203a9d72cee0e",
+    "url": "/nes/static/js/2.edc84839.chunk.js"
+  },
+  {
+    "revision": "ce788a1fa508dab3627efbe6528a9a6f",
+    "url": "/nes/static/js/2.edc84839.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f6c6fb8d1a62a32d5b0",
+    "url": "/nes/static/js/main.2a2a7150.chunk.js"
+  },
+  {
+    "revision": "fcee2c4bd540c07e9fde",
+    "url": "/nes/static/js/runtime-main.b1275b39.js"
+  }
+]);
